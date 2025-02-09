649b2af510b2a7ad2a8cde502395ecddf480a19c
Vitória Amaral <vitoriaamaral329@gmail.com>
2025-02-09 14:32:12 -0300

    primeiro

---
+++ README.md
@@ -0,0 +1,73 @@
+DFA
+{
+  "states": ["q0", "q1", "q2"],
+  "input_symbols": ["0", "1"],
+  "transitions": {
+    "q0": {"0": "q0", "1": "q1"},
+    "q1": {"0": "q0", "1": "q2"},
+    "q2": {"0": "q2", "1": "q1"}
+  },
+  "initial_state": "q0",
+  "final_states": ["q1"],
+  "word": "000111"
+}
+
+MT (palavras que começam com 0 e seguidas pela mesma quantidade de 1's)
+{
+  "states": ["q0", "q1", "q2", "q3", "q4"],
+  "input_symbols": ["0", "1"],
+  "tape_symbols": ["0", "1", "x", "y", "."],
+  "transitions": {
+    "q0": {
+      "0": ["q1", "x", "R"],
+      "y": ["q3", "y", "R"]
+    },
+    "q1": {
+      "0": ["q1", "0", "R"],
+      "1": ["q2", "y", "L"],
+      "y": ["q1", "y", "R"]
+    },
+    "q2": {
+      "0": ["q2", "0", "L"],
+      "x": ["q0", "x", "R"],
+      "y": ["q2", "y", "L"]
+    },
+    "q3": {
+      "y": ["q3", "y", "R"],
+      ".": ["q4", ".", "R"]
+    }
+  },
+  "initial_state": "q0",
+  "blank_symbol": ".",
+  "final_states": ["q4"],
+  "word": "0011"
+}
+
+PDA
+
+{
+  "states": ["q0", "q1", "q2", "q3"],
+  "input_symbols": ["a", "b"],
+  "stack_symbols": ["0", "1"],
+  "transitions": {
+    "q0": {
+      "a": ["q1", "1", "0"]
+    },
+    "q1": {
+      "a": ["q1", "1", "1"],
+      "b": ["q2", "", "1"]
+    },
+    "q2": {
+      "b": ["q2", "", "1"],
+      "": ["q3", "0", ""]
+    }
+  },
+  "initial_state": "q0",
+  "initial_stack_symbol": "0",
+  "final_states": ["q3"],
+  "acceptance_mode": "final_state",
+  "word": "aabb"
+}
+
+
+
---
+++ main.py
@@ -0,0 +1,115 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Set, Tuple, Dict, List
+
+from automata.tm.dtm import DTM  # Importando máquina de turing determinística
+from automata.pda.dpda import DPDA  # Importando autômata de pilha determinística
+from automata.fa.dfa import DFA  # Importando autômata finito determinístico
+
+class FiniteAutomataDeterministic(BaseModel):
+    states: Set[str]
+    input_symbols: Set[str]
+    transitions: Dict[str, Dict[str, str]]
+    initial_state: str
+    final_states: Set[str]
+    word: str  # Palavra para ser testada
+
+
+class TuringMachine(BaseModel):
+    states: Set[str]
+    input_symbols: Set[str]
+    tape_symbols: Set[str]
+    transitions: Dict[str, Dict[str, Tuple[str, str, str]]]
+    initial_state: str
+    blank_symbol: str
+    final_states: Set[str]
+    word: str  # Palavra para ser testada
+    
+class PushdownAutomata(BaseModel):
+    states: Set[str]
+    input_symbols: Set[str]
+    stack_symbols: Set[str]
+    transitions: Dict[str, Dict[str, Tuple[str, str, str]]]
+    initial_state: str
+    initial_stack_symbol: str
+    final_states: Set[str]
+    acceptance_mode: str
+    word: str  # Palavra para ser testada
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "API de Autômatos em funcionamento!"}
+
+@app.post("/turing/")
+def validate_turing_machine(turing: TuringMachine): 
+    dtm = DTM(
+        states=turing.states,
+        input_symbols=turing.input_symbols,
+        tape_symbols=turing.tape_symbols,
+        transitions=turing.transitions,
+        initial_state=turing.initial_state,
+        blank_symbol=turing.blank_symbol,
+        final_states=turing.final_states,
+    )
+    
+    aceita = dtm.accepts_input(turing.word)
+    return {
+        "states": turing.states,
+        "input_symbols": turing.input_symbols,
+        "tape_symbols": turing.tape_symbols,
+        "transitions": turing.transitions,
+        "initial_state": turing.initial_state,
+        "blank_symbol": turing.blank_symbol,
+        "final_states": turing.final_states,
+        "word": turing.word, 
+        "result": "aceita" if aceita else "rejeitada"
+    }
+
+@app.post("/finite/")
+def validate_finite_automata_deterministic(finite: FiniteAutomataDeterministic): 
+    dfa = DFA(
+        states=finite.states,
+        input_symbols=finite.input_symbols,
+        transitions=finite.transitions,
+        initial_state=finite.initial_state,
+        final_states=finite.final_states,
+    )
+    
+    aceita = dfa.accepts_input(finite.word)
+    return {
+        "states": finite.states,
+        "input_symbols": finite.input_symbols,
+        "transitions": finite.transitions,
+        "initial_state": finite.initial_state,
+        "final_states": finite.final_states,
+        "word": finite.word, 
+        "result": "aceita" if aceita else "rejeitada"
+    }
+
+@app.post("/pushdown/")
+def validate_pushdown_automata(pushdown: PushdownAutomata): 
+    dpda = DPDA(
+        states = pushdown.states,
+        input_symbols = pushdown.input_symbols,
+        stack_symbols = pushdown.stack_symbols,
+        transitions = pushdown.transitions,
+        initial_state = pushdown.initial_state,
+        initial_stack_symbol = pushdown.initial_stack_symbol,
+        final_states = pushdown.final_states,
+        acceptance_mode = pushdown.acceptance_mode,
+    )
+    
+    aceita = dpda.accepts_input(pushdown.word)
+    return {
+        "states": pushdown.states,
+        "input_symbols": pushdown.input_symbols,
+        "stack_symbols": pushdown.stack_symbols,
+        "transitions": pushdown.transitions,
+        "initial_state": pushdown.initial_state,
+        "final_states": pushdown.final_states,
+        "word": pushdown.word, 
+        "result": "aceita" if aceita else "rejeitada",
+    }
+
